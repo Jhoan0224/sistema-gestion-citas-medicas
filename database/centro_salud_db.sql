4bb84f7f9547efb80590ffddbde6e4dcdd94f1d3
time2code <time2code@code2time.dev>
2026-03-03 15:27:31 -0600

    Script de DB en MySQL y Script de configuracion

--- database/centro_salud_db.sql
+++ database/centro_salud_db.sql
@@ -3,14 +3,42 @@ create database if not exists centro_salud_db;
 use centro_salud_db;
 
 create table usuario(
-	id_usuario int auto_increment primary key,
+	id int auto_increment primary key,
 	nombre varchar(50),
 	apellido varchar(50),
 	dui varchar(10),
-	fecha_naciento date,
+	fecha_nacimiento date,
 	email varchar(256),
 	pass varchar(256),
 	zona_residencia varchar(128),
-	estado_laboral_formal boolean,
+	estado_laboral_formal boolean,	
 	condicion_medica boolean
+);
+
+create table estado_cita(
+	id int auto_increment primary key,
+	nombre varchar(50)
+);
+
+create table especialidad(
+	id int auto_increment primary key,
+	nombre varchar(60),
+	atencion_diaria_max int
+);
+
+
+create table cita(
+  id int auto_increment primary key,
+  nombre varchar(50),
+  motivo varchar(300),
+  fecha_atencion datetime,
+  fecha_hora_registro timestamp default CURRENT_TIMESTAMP(),
+  id_estado_cita int,
+  id_usuario int,
+  id_especialidad int,
+  constraint fkUsuario_Cita foreign key (id_usuario) references usuario(id)
+  	on delete cascade,
+  constraint fkEstadoCita_Cita foreign key (id_estado_cita) references estado_cita(id)
+  	on delete cascade,
+	constraint fkEspecialidad_Cita foreign key (id_especialidad) references especialidad(id);
 );
